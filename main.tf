7073aa9cf00c207568e91dfc058c11c681c9d7c5
Jonathan Magee <120496483+jmagee70@users.noreply.github.com>
2024-05-16 14:11:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,23 +14,23 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-example-bucket-12345"
+resource "aws_s3_bucket" "TFC_Prisma" {
+  bucket = "TFC-bucket-Prisma"
   acl    = "private"
 
   tags = {
-    Name        = "example-s3-bucket"
-    Environment = "Dev"
+    Name        = "TFC-s3-bucket"
+    Environment = "Prisma"
   }
 }
 
-resource "null_resource" "example_run_task" {
+resource "null_resource" "TFC_run_task" {
   provisioner "local-exec" {
     command = "echo 'Run task executed during apply phase' > /tmp/terraform_run_task.txt"
   }
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
+  value = aws_s3_bucket.TFC.bucket
 }
 
