e10856ce6144175bec11bf5c5a8315f0c94a1ee5
Jonathan Magee <120496483+jmagee70@users.noreply.github.com>
2024-05-20 08:24:24 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "PAN-Black-Belts-Team"
+
+    workspaces {
+      name = "TFC"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
 module "ec2_instance" {
 source = "terraform-aws-modules/ec2-instance/aws"
 
